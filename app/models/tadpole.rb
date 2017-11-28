7fb3472b721b4b56766980a1d4d02b3b6e1200b0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-28 19:49:15 +0000

    automatically backed up by learn

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,4 +1,10 @@
 class Tadpole < ActiveRecord::Base
   belongs_to :frog
   delegate :pond, :to => :frog, :allow_nil => true
+
+  def self.metamorphose
+    self.color
+    self.name
+    self.pond_id
+  end
 end
