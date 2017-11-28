635ed0f00e9dc606b6f0aee92d367754fae50184
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-28 19:29:01 +0000

    automatically backed up by learn

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,5 +1,7 @@
 class CreateFrogs < ActiveRecord::Migration
   create_table :frogs do |f| 
-    f.sting :name
-  end
+      f.string :name
+      f.string :color
+      f.integer :pond_id
+    end
 end
