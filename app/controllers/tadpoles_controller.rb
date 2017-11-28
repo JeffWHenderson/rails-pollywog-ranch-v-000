7012ea923218a00b1fe9706b626ebcf652661fda
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-28 20:22:45 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,9 +2,9 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
   def metamorphose
-    @frog = Frog.create(:name => @tadpole.name, :color => @tadpole.color, :pond => @tadpole.pond)
-    @tadpole.destroy
-    redirect_to ("/frogs/#{@frog.id}")
+    # @frog = Frog.create(:name => @tadpole.name, :color => @tadpole.color, :pond => @tadpole.pond)
+    # @tadpole.destroy
+    # redirect_to ("/frogs/#{@frog.id}")
   end
 
   def index
