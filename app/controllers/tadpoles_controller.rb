ceb0193bef59aacd318770190824123af0368ea3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-28 20:34:44 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -6,7 +6,7 @@ class TadpolesController < ApplicationController
     @frog = Frog.create(:name => @tadpole.name, :color => @tadpole.color, :pond => @tadpole.pond)
     @tadpole.destroy
 
-    redirect_to(/frogs/"#{@frog.id}")
+    redirect_to frogs_path(@frog)
   end
 
   def index
