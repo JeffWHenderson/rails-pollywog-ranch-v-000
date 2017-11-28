1aefd360dbfb9c3249a1362230f55674ba668419
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-28 20:17:25 +0000

    automatically backed up by learn

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -1,13 +1,4 @@
 class Tadpole < ActiveRecord::Base
   belongs_to :frog
   delegate :pond, :to => :frog, :allow_nil => true
-
-  def metamorphose
-   name = self.name
-   color = self.color
-   pond = Frog.find_by_id(self.frog_id).pond
-
-   Frog.create(name: name, color: color, pond: pond)
-   self.destroy
-  end
 end
